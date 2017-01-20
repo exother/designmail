ec6546ed7958cc1638f61fb29611a04e7b282b5d
Michał Zezyk <zezyk.michal@gmail.com>
2017-01-20 07:19:25 +0100

    reference changed to email

--- db/migrate/20170120013943_create_campaign_mails.rb
+++ db/migrate/20170120013943_create_campaign_mails.rb
@@ -2,7 +2,7 @@ class CreateCampaignMails < ActiveRecord::Migration[5.0]
   def change
     create_table :campaign_mails do |t|
       t.references :campaign
-      t.references :mail
+      t.references :email
       t.integer :status
 
       t.timestamps
