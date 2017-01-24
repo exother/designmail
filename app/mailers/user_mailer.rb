e450c4144ab0e49b0c1889d94ef845ae18b72c9d
Michał Zezyk <zezyk.michal@gmail.com>
2017-01-24 05:49:51 +0100

    mailer added

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,31 @@
+class UserMailer < ApplicationMailer
+
+  after_action :set_delivery_setting
+  after_filter :save_campaign_mail
+
+  def mailer_message(user, campaign)
+    @mail_body = campaign.content
+    @campaign = campaign
+    @user = user
+    @setting = campaign.setting
+    mail(to: user.email, subject: 'test')
+  end
+
+  private
+
+  def set_delivery_setting
+    mail.delivery_method.settings = {
+        authentication: "plain",
+        address: @setting.host,
+        port: 587,
+        domain: "dotd.pl",
+        user_name: @setting.user,
+        password: @setting.password,
+        enable_starttls_auto: true
+    }
+  end
+
+  def save_campaign_mail
+    CampaignMail.where(campaign_id: @campaign.id, email_id: @user.id).take.update status: 1
+  end
+end
